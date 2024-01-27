15ff754bace298ad422d4bd3f0e98e84c9f35a13
ab00s1 <157624328+ab00s1@users.noreply.github.com>
2024-01-27 23:32:26 +0530

    Add files via upload

---
+++ player.h
@@ -0,0 +1,17 @@
+#include <string>
+
+
+
+class Player {
+public :
+
+	Player();
+	int id;
+	std::string name;
+	int runsScored;
+	int ballsPlayed;
+	int ballsBowled;
+	int runsGiven;
+	int wicketTaken;
+
+};
